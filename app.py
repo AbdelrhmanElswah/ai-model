3d7a38ec42a862a4511b19bd1b4c890988e9a9e3
Ahmed Abouemaira <a.aboemira1@gmail.com>
2024-06-24 12:40:25 +0300

    update

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from models.brain_tumor_model import BrainTumorModel
 from models.covid19_model import Covid19Model
 from utils.preprocess import preprocess_image
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 # Initialize your models
 brain_tumor_model = BrainTumorModel()
 covid19_model = Covid19Model()
@@ -36,8 +36,11 @@ def predict_covid19():
     if file.filename == '':
         return jsonify({'code': 400, 'status': 'error', 'data': 'No file selected'}), 400
     if file:
-        image = preprocess_image(file, target_size=(224, 224))
-        prediction, top_two = covid19_model.predict(image)
+        file_path = request.files['file'].stream  # Use file stream for direct access
+        prediction, top_two = covid19_model.predict(file_path)
+        print(prediction)
+        print(top_two)
+
         return jsonify({
             'code': 200,
             'status': 'success',
--- models/covid19_model.py
+++ models/covid19_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+import cv2
+from PIL import Image
 
 class Covid19Model:
     def __init__(self):
@@ -7,8 +9,23 @@ class Covid19Model:
         self.model = tf.keras.models.load_model(self.model_path)
         self.class_names = ['COVID', 'Lung_Opacity', 'Normal', 'Viral Pneumonia']
 
-    def predict(self, image):
-        prediction = self.model.predict(np.expand_dims(image, axis=0))[0]
+    def preprocess_image(self, image_path):
+        # Load the image using PIL and convert to RGB
+        pil_image = Image.open(image_path).convert('RGB')
+        # Convert the image to OpenCV format
+        open_cv_image = np.array(pil_image)
+        open_cv_image = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
+        # Resize the image to the target size
+        image_resized = cv2.resize(open_cv_image, (224, 224))
+        # Normalize the image if needed
+        image_array = image_resized / 255.0
+        # Expand dimensions to match model input
+        image_array = np.expand_dims(image_array, axis=0)
+        return image_array
+
+    def predict(self, image_path):
+        image = self.preprocess_image(image_path)
+        prediction = self.model.predict(image)[0]
         top_two_indices = np.argsort(prediction)[-2:][::-1]
         top_two = [{'class_name': self.class_names[idx], 'percent': f'{prediction[idx]:.2%}'} for idx in top_two_indices]
         return prediction, top_two
